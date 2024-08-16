202c2bbb9678a637e0b40276bacb79dc8c620c01
Robert Andruschow <robert@andruschow.de>
2024-08-16 16:33:30 +0200

    Initial version with watching namespaces and pods, a Dockerfile and rbac config.

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# syntax=docker/dockerfile:1
+
+FROM golang:1.22 as base
+
+# Set destination for COPY
+WORKDIR /app
+
+# Download Go modules
+COPY go.mod go.sum ./
+RUN go mod download
+
+# Copy the source code. Note the slash at the end, as explained in
+# https://docs.docker.com/reference/dockerfile/#copy
+COPY cmd/kube-advisor-agent/*.go ./
+
+# Build
+RUN CGO_ENABLED=0 GOOS=linux go build -o /kube-advisor-agent
+
+FROM scratch
+
+COPY --from=base /kube-advisor-agent /kube-advisor-agent
+
+# Run
+CMD ["/kube-advisor-agent"]
---
+++ cmd/kube-advisor-agent/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+    "context"
+    "os"
+    "sync"
+
+    log "github.com/sirupsen/logrus"
+    corev1 "k8s.io/api/core/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/apimachinery/pkg/watch"
+    "k8s.io/client-go/kubernetes"
+    "k8s.io/client-go/tools/cache"
+    "k8s.io/client-go/tools/clientcmd"
+    toolsWatch "k8s.io/client-go/tools/watch"
+)
+
+var (
+    config, _    = clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
+    clientset, _ = kubernetes.NewForConfig(config)
+)
+
+func watchNamespaces() {
+
+    watchFunc := func(options metav1.ListOptions) (watch.Interface, error) {
+        timeOut := int64(60)
+		log.Info("Starting watching namespaces")
+        return clientset.CoreV1().Namespaces().Watch(context.Background(), metav1.ListOptions{TimeoutSeconds: &timeOut})
+    }
+
+    watcher, _ := toolsWatch.NewRetryWatcher("1", &cache.ListWatch{WatchFunc: watchFunc})
+
+    for event := range watcher.ResultChan() {
+        item := event.Object.(*corev1.Namespace)
+
+        switch event.Type {
+        case watch.Modified:
+        case watch.Bookmark:
+        case watch.Error:
+        case watch.Deleted:
+        case watch.Added:
+            processNamespace(item.GetName())
+        }
+    }
+}
+
+func processNamespace(namespace string) {
+    log.Info("Some processing for newly created namespace : ", namespace)
+}
+
+func watchPods() {
+
+    watchFunc := func(options metav1.ListOptions) (watch.Interface, error) {
+        timeOut := int64(60)
+		log.Info("Starting watching namespaces")
+        return clientset.CoreV1().Pods("").Watch(context.Background(), metav1.ListOptions{TimeoutSeconds: &timeOut})
+    }
+
+    watcher, _ := toolsWatch.NewRetryWatcher("1", &cache.ListWatch{WatchFunc: watchFunc})
+
+    for event := range watcher.ResultChan() {
+        item := event.Object.(*corev1.Pod)
+
+        switch event.Type {
+        case watch.Modified:
+        case watch.Bookmark:
+        case watch.Error:
+        case watch.Deleted:
+        case watch.Added:
+            processPod(item.GetName())
+        }
+    }
+}
+
+func processPod(pod string) {
+    log.Info("Some processing for newly created pod: ", pod)
+}
+
+func main() {
+    var wg sync.WaitGroup
+    go watchNamespaces()
+	go watchPods()
+    wg.Add(1)
+    wg.Wait()
+}
